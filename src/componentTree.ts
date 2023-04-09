8ece495f042aaa09638e442b960eec79b5f938b5
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-04-09 18:57:59 +0800

    chore: remove lastIndex variable

--- src/componentTree.ts
+++ src/componentTree.ts
@@ -4,10 +4,9 @@ type Provider<Props = {}> = [React.ElementType, Props?]
 
 export function componentTree(providers: Provider[]) {
   return ({ children }: { children: JSX.Element }) => {
-    const lastIndex = providers.length - 1
     let currentChildren = children
 
-    for (let i = lastIndex; i >= 0; i--) {
+    for (let i = providers.length - 1; i >= 0; i--) {
       const [component, props = {}] = providers[i]!
       currentChildren = createElement(component, props, currentChildren)
     }
