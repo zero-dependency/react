8a00540d2d13c3bb2fba48f6209e41737bafffaf
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-04-14 22:30:48 +0800

    refactor `useStorage`

--- src/useStorage.ts
+++ src/useStorage.ts
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react'
+import { useCallback, useLayoutEffect, useRef, useState } from 'react'
 import { WebStorage } from '@zero-dependency/storage'
 import type { ExcludeFunction, StorageOptions } from '@zero-dependency/storage'
 
@@ -8,43 +8,29 @@ export function useStorage<T>(
   storage: Storage,
   options?: StorageOptions<T>
 ) {
-  const webStorage = useRef(
-    (
-      key: string,
-      initialValue: ExcludeFunction<T>,
-      options?: StorageOptions<T>
-    ) => new WebStorage(key, initialValue, storage, options)
-  )
+  const webStorage = useRef<WebStorage<T>>()
+  const [value, setValue] = useState<T>()
 
-  const [state, setState] = useState<T>(
-    () => webStorage.current(key, initialValue, options).values
-  )
+  useLayoutEffect(() => {
+    webStorage.current = new WebStorage(key, initialValue, storage, options)
+    setValue(webStorage.current.values)
+  }, [key])
 
   const set = useCallback(
     (value: T) => {
-      webStorage.current(key, initialValue, options).write(value)
-      setState(value)
+      webStorage.current!.write(value)
+      setValue(value)
     },
-    [
-      key,
-      initialValue,
-      options,
-      setState
-    ]
+    [webStorage, setValue]
   )
 
   const reset = useCallback(() => {
-    webStorage.current(key, initialValue, options).reset()
-    setState(initialValue)
-  }, [
-    key,
-    initialValue,
-    options,
-    setState
-  ])
+    webStorage.current!.reset()
+    setValue(initialValue)
+  }, [webStorage, setValue])
 
   return [
-    state,
+    value,
     set,
     reset
   ] as const
