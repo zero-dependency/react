5ee636a11c18092b52fe0944535d7091d199bdcb
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-04-20 17:24:48 +0800

    feat: drop umd support

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import { name } from './package.json'
+import { dependencies, name } from './package.json'
 
 export default defineConfig({
   plugins: [react(), dts()],
@@ -11,28 +11,13 @@ export default defineConfig({
     minify: true,
     target: 'esnext',
     lib: {
-      formats: [
-        'cjs',
-        'es',
-        'umd'
-      ],
+      formats: ['cjs', 'es'],
       entry: resolve(__dirname, 'src/index.ts'),
       fileName: (format) => `index.${format}.js`,
       name
     },
     rollupOptions: {
-      external: [
-        'react',
-        '@zero-dependency/cookie',
-        '@zero-dependency/storage'
-      ],
-      output: {
-        globals: {
-          react: 'React',
-          '@zero-dependency/cookie': 'cookie',
-          '@zero-dependency/storage': 'storage'
-        }
-      }
+      external: Object.keys(dependencies)
     }
   }
 })
